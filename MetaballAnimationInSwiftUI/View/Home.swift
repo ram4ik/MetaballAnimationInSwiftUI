499dfdde852150affd34bccbc44f0bd30fd7dacf
Ramill <ramill_ibragimov@icloud.com>
2022-09-22 15:27:22 +0300

    with Gradient mask

--- MetaballAnimationInSwiftUI/View/Home.swift
+++ MetaballAnimationInSwiftUI/View/Home.swift
@@ -19,34 +19,41 @@ struct Home: View {
     
     @ViewBuilder
     func SingleMetaBall() -> some View {
-        Canvas {context, size in
-            context.addFilter(.alphaThreshold(min: 0.5, color: .white))
-            context.addFilter(.blur(radius: 30))
-            context.drawLayer { ctx in
-                for index in [1, 2] {
-                    if let resolvedView = context.resolveSymbol(id: index) {
-                        ctx.draw(resolvedView, at: CGPoint(x: size.width / 2, y: size.height / 2))
+        
+        Rectangle()
+            .fill(.linearGradient(colors: [Color("Gradient1"), Color("Gradient2")], startPoint: .top, endPoint: .bottom))
+            .mask {
+                Canvas {context, size in
+                    context.addFilter(.alphaThreshold(min: 0.5, color: .white))
+                    context.addFilter(.blur(radius: 30))
+                    context.drawLayer { ctx in
+                        for index in [1, 2] {
+                            if let resolvedView = context.resolveSymbol(id: index) {
+                                ctx.draw(resolvedView, at: CGPoint(x: size.width / 2, y: size.height / 2))
+                            }
+                        }
                     }
+                } symbols: {
+                    Ball()
+                        .tag(1)
+                    
+                    Ball(offset: dragOffset)
+                        .tag(2)
                 }
             }
-        } symbols: {
-            Ball()
-                .tag(1)
-            
-            Ball(offset: dragOffset)
-                .tag(2)
-        }
-        .gesture(
-            DragGesture()
-                .onChanged({ value in
-                    dragOffset = value.translation
-                })
-                .onEnded({ _ in
-                    withAnimation(.interactiveSpring(response: 0.6, dampingFraction: 0.7, blendDuration: 0.7)) {
-                        dragOffset = .zero
-                    }
-                })
-        )
+        
+        
+            .gesture(
+                DragGesture()
+                    .onChanged({ value in
+                        dragOffset = value.translation
+                    })
+                    .onEnded({ _ in
+                        withAnimation(.interactiveSpring(response: 0.6, dampingFraction: 0.7, blendDuration: 0.7)) {
+                            dragOffset = .zero
+                        }
+                    })
+            )
     }
     
     @ViewBuilder
