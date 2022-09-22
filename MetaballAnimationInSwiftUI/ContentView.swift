b3468dd0f68db4659616433de9cbc6131ba7a406
Ramill <ramill_ibragimov@icloud.com>
2022-09-22 15:05:00 +0300

    Initial Commit

---
+++ MetaballAnimationInSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MetaballAnimationInSwiftUI
+//
+//  Created by Ramill Ibragimov on 22.09.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MetaballAnimationInSwiftUI/MetaballAnimationInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetaballAnimationInSwiftUIApp.swift
+//  MetaballAnimationInSwiftUI
+//
+//  Created by Ramill Ibragimov on 22.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct MetaballAnimationInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
